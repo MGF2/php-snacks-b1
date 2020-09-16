e601d13b6115c8ae848b0c030cd6cd2087bbcc18
MGF2 <fioremartina@hotmail.com>
2020-09-16 16:18:14 +0200

    Snack2 ridotto+prove

--- snack2/index.php
+++ snack2/index.php
@@ -8,24 +8,44 @@ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” 
  $mail = $_GET['mail'];
  $age = $_GET['age'];
 
+ //RIDOTTO
+ if ( empty($name) || empty($mail) || empty($age) ) {
+   $text = 'Accesso negato';
+ } else {
+   if ( (strlen($name) > 3) && ( (strpos($mail, '@') !== false) && (strpos($mail, '.') !== false) ) && (intval($age) == true) ){
+     $text = 'Accesso consentito';
+   } else {
+     $text = 'Accesso negato';
+   }
+ }
 
-if ( empty($name) || empty($mail) || empty($age) ) {
-  $text = 'Accesso negato. Compila tutti i campi';  //Compila tutti i campi
-} else {
-  if (strlen($name) <= 3) {
-    $text = 'Accesso negato. Nome troppo corto';    //Controllo lunghezza
-  } else {
-    if ( (strpos($mail, '@') === false) && (strpos($mail, '.') === false) ){
-      $text = 'Accesso negato. Controllo mail';     //Contrllo @ e .
-    } else {
-      if (intval($age) == true) {                   //Controllo intero
-        $text = 'Accesso consentito.';
-      } else {
-        $text = 'Accesso negato.';
-      }
-    }
-  }
-}
+//COSTRUZIONE
+// if ( empty($name) || empty($mail) || empty($age) ) {
+//   $text = 'Accesso negato. Compila tutti i campi';  //Compila tutti i campi
+// } else {
+//   if (strlen($name) <= 3) {
+//     $text = 'Accesso negato. Nome troppo corto';    //Controllo lunghezza
+//   } else {
+//     if ( (strpos($mail, '@') === false) && (strpos($mail, '.') === false) ){
+//       $text = 'Accesso negato. Controllo mail';     //Contrllo @ e .
+//     } else {
+//       if (intval($age) == true) {                   //Controllo intero
+//         $text = 'Accesso consentito.';
+//       } else {
+//         $text = 'Accesso negato.';
+//       }
+//     }
+//   }
+// }
+
+
+//PROVA IF UNICO
+// if ( ( ( empty($name) || empty($mail) || empty($age) ) == false ) && ( (strlen($name) > 3) && ( (strpos($mail, '@') !== false) && (strpos($mail, '.') !== false) ) && (intval($age) == true) ) ){
+//     $text = 'Accesso consentito';
+// } else {
+//   $text = 'Accesso negato';
+// }
+
 
 
 echo $text;
